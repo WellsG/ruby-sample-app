72c996e2c1ae3559426e444b29e7c394d52c7beb
Wenjie Guo <wguo@redhat.com>
2016-10-29 18:31:22 +0800

    add testcase for model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,29 +12,34 @@
 #
 
 class User < ApplicationRecord
-attr_accessor :remember_token
-has_secure_password
-validates(:name, presence: true, length: {maximum:50})
-validates :email, presence: true
-has_many :microposts, dependent: :destroy
+  
+  attr_accessor :remember_token
 
-def User.digest(string)
-  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+  before_save { email.downcase! }
+  #has_secure_password
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255},
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_many :microposts, dependent: :destroy
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
-end
+  end
 
-def User.new_token
+  def User.new_token
     SecureRandom.urlsafe_base64
-end
+  end
 
-def remember
+  def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-end
+  end
 
-def authenticated?(remember_token)
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
-end
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 
 end
